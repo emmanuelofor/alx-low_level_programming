04c23e7b6d821174195d36825aa57e7ec1bcb4e8
emmanuelofor <emmanuel.ofori@kibo.school>
2022-12-08 12:56:21 -0800

    Task_5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - print a c programme using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
