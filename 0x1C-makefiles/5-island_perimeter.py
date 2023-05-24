59b99cf0f6c6052d80476e96ce31ff870dbe18bd
emmanuelofor <emmanuel.ofori@kibo.school>
2023-05-24 06:04:17 -0700

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""This module is responsible for determining the perimeter of an island within a grid."""
+"""This module is responsible for calculating the perimeter of an island in a grid."""
 
 
 def num_water_neighbors(grid, i, j):
-    """Calculates and returns the quantity of water-adjacent cells for a given cell in the grid."""
+    """Calculates and returns the quantity of water neighbors a cell has in a grid."""
 
     num = 0
 
-    # Increment count if the cell is at the top edge or its upper neighbor is water
     if i <= 0 or not grid[i - 1][j]:
         num += 1
-    # Increment count if the cell is at the left edge or its left neighbor is water
     if j <= 0 or not grid[i][j - 1]:
         num += 1
-    # Increment count if the cell is at the right edge or its right neighbor is water
     if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
         num += 1
-    # Increment count if the cell is at the bottom edge or its lower neighbor is water
     if i >= len(grid) - 1 or not grid[i + 1][j]:
         num += 1
 
@@ -24,13 +20,11 @@ def num_water_neighbors(grid, i, j):
 
 
 def island_perimeter(grid):
-    """Calculates and returns the total perimeter of the island represented in the grid."""
+    """Calculates and returns the total perimeter of the island in grid."""
 
     perimeter = 0
-    # Loop through each cell in the grid
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            # If the cell is part of the island, add the number of its water neighbors to the total perimeter
             if grid[i][j]:
                 perimeter += num_water_neighbors(grid, i, j)
 
