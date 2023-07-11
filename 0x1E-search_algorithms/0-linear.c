5fdc55360d574dbe7cbaa20ca421a5903668a8e9
emmanuelofor <emmanuel.ofori@kibo.school>
2023-07-11 06:57:42 -0700

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,45 +1,23 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in an integer array using a binary
- * search algorithm, not guaranteed to return lowest index if `value` appears
- * twice in `array`
- * @array: pointer to first element of array to seach
- * @size: number of elements in array
+ * linear_search - searches for a value in
+ * an array of integers using the Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size:  number of elements in array
  * @value: value to search for
- *
- * Return: index containing `value`, or -1 if `value` not found or
- * `array` is NULL
+ * Return: 1 where value is located or -1 otherwise
  */
-
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	int low, mid, high;
-	int x;
+	size_t i = 0;
 
-	if (array == NULL)
+	while (array && i < size)
 	{
-		return (-1);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
 	}
-
-	low = 0;
-	high = size - 1;
-
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
-
-		printf("Searching in array: ");
-		for (x = low; x <= high; x++)
-			printf("%i%s", array[x], x == high ? "\n" : ", ");
-
-		if (array[mid] < value)
-			low = mid + 1;
-		else if (array[mid] > value)
-			high = mid - 1;
-		else
-			return (mid);
-	}
-
 	return (-1);
 }
